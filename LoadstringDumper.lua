cd523cf335cf1d6221b61086007b7075bb02b823
Isaac Newton The 2nd <93484868+n3wozz@users.noreply.github.com>
2023-03-11 16:57:05 +0000

    Gay

---
+++ LoadstringDumper.lua
@@ -0,0 +1,23 @@
+-- Saves everything passed through loadstring to your workspace folder
+-- Note: this can lag pretty bad
+
+local Folder = "LoadstringDump"
+local Folder_Path = Folder.."/".."Data"
+
+if not isfolder(Folder) then
+    makefolder(Folder)
+end
+
+
+if not isfolder(Folder_Path) then
+    makefolder(Folder_Path)
+end
+
+local old; old = hookfunction(loadstring, function(...)
+
+    writefile(Folder_Path.."/".."Log - "..string.gsub(tostring(math.random(1, 1e9) / os.time()), "[%s%p]", "")..".txt",
+
+    (...)
+)
+    return old(...)
+end)
