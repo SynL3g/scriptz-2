6ba8698942b7c25b5126eb9ebafd39f5add64385
Isaac Newton The 2nd <93484868+n3wozz@users.noreply.github.com>
2023-03-11 17:08:43 +0000

    Boobs
    yeah

--- LoadstringDumper.lua
+++ LoadstringDumper.lua
@@ -14,13 +14,20 @@ if not isfolder(Folder_Path) then
     makefolder(Folder_Path)
 end
 
+local notify = function(title, time)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = title,
+        Text = "",
+        Duration = time
+    })
+end
+
 local old; old = hookfunction(loadstring, function(...)
 
-    writefile(
-            
-    Folder_Path.."/".."Log - "..string.gsub(tostring(math.random(1, 1e9) / os.time()), "[%s%p]", "")..".txt",
+    writefile(Folder_Path.."/".."Log - "..string.gsub(tostring(math.random(1, 1e9) / os.time()), "[%s%p]", "")..".txt",
 
     (...)
 )
+    notify("Loadstring Saved!", 3)
     return old(...)
 end)
