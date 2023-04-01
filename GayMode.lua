91a43cb8fd07d4f96cb56e09bbda2d100f02da9b
SynL3g <129326842+SynL3g@users.noreply.github.com>
2023-04-01 13:13:54 +0100

    Glock My Cock

---
+++ GayMode.lua
@@ -0,0 +1,11 @@
+-- i use a whitelist
+local players = game:FindService("Players")
+local Perm_NBC = string.match(game:HttpGet("https://inventory.roblox.com/v1/users/"..v.UserId.."/items/GamePass/".. 66254), 66254)
+local Perm_BC = string.match(game:HttpGet("https://inventory.roblox.com/v1/users/"..v.UserId.."/items/GamePass/".. 64354), 64354)
+
+players.PlayerAdded:Connect(function(v)
+    if Perm_NBC or Perm_BC then
+        game.Players:Chat("h \n\n\n\n\n\n\n\n\nPerm Moment")
+        game.Players:Chat("kick "..v.name)
+    end 
+end)
